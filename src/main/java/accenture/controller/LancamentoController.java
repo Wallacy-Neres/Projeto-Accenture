01737dc842feeee11b26e43c5ddf92b00372b87d
Wendell <wendell_rpg@hotmail.com>
2021-03-03 17:10:08 -0300

    Removida validação desnecessária do JWT nos Controllers.

--- src/main/java/accenture/controller/LancamentoController.java
+++ src/main/java/accenture/controller/LancamentoController.java
@@ -5,12 +5,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import accenture.model.Lancamento;
-import accenture.service.AutenticacaoService;
 import accenture.service.LancamentoService;
 
 
@@ -20,15 +18,10 @@ public class LancamentoController {
 
 	@Autowired
 	private LancamentoService lancamentoService;
-	
-	@Autowired
-	private AutenticacaoService autenticacao;
 
 	@PostMapping("/cadastrar")
-	public ResponseEntity<Lancamento> PostCadastrar(@RequestBody Lancamento lancamento, @RequestHeader String Authorization){
-		if (!Authorization.isEmpty() && autenticacao.validaToken(Authorization))
-			return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoService.CadastrarLancamento(lancamento));
-		return null;
+	public ResponseEntity<Lancamento> PostCadastrar(@RequestBody Lancamento lancamento){
+		return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoService.CadastrarLancamento(lancamento));
 
 	}
 	
--- src/main/java/accenture/controller/PlanoContaController.java
+++ src/main/java/accenture/controller/PlanoContaController.java
@@ -8,13 +8,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import accenture.DTO.PlanoContaDTO;
 import accenture.exception.UsuarioNaoEncontradoException;
-import accenture.service.AutenticacaoService;
 import accenture.service.PlanoContaService;
 
 @RestController
@@ -24,19 +22,13 @@ public class PlanoContaController {
 	@Autowired
 	private PlanoContaService planoContaService;
 	
-	@Autowired
-	private AutenticacaoService autenticacao;
-	
 	@PostMapping("/cadastrar")
-	public ResponseEntity<PlanoContaDTO> PostCadastrar(@RequestBody PlanoContaDTO planoContaDTO, @RequestHeader String Authorization) throws UsuarioNaoEncontradoException{	
-		if (!Authorization.isEmpty() && autenticacao.validaToken(Authorization)){
-			PlanoContaDTO usuario = planoContaService.CadastrarPlanoConta(planoContaDTO);
-			if(usuario != null) 
-				return ResponseEntity.status(HttpStatus.CREATED).body(usuario);
-			else
-				return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-		}
-		return null;
+	public ResponseEntity<PlanoContaDTO> PostCadastrar(@RequestBody PlanoContaDTO planoContaDTO) throws UsuarioNaoEncontradoException{	
+		PlanoContaDTO usuario = planoContaService.CadastrarPlanoConta(planoContaDTO);
+		if(usuario != null) 
+			return ResponseEntity.status(HttpStatus.CREATED).body(usuario);
+		else
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 	}
 	
 	@GetMapping
